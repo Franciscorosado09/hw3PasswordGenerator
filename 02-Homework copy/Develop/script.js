89acc93861f2fe40b8f9693b3b4688e8d79044a4
Francisco Rosado <Franciscorosado09@gmail.com>
2020-11-20 21:15:42 -0500

    well this is going to be something

---
+++ 02-Homework copy/Develop/script.js
@@ -0,0 +1,59 @@
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+
+//Variables
+
+var lowercase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+var uppercase
+
+
+// var uppercase = ()
+var numbers = math.floor(math.random() * 9)+1;
+
+
+
+
+// var specialcharacters= ()
+// var numberofcharacters= ()
+
+// var verifyLC = confirm("Would you like lower-case letters?")
+// var verifyUC = confirm ("Would you like upper-case letters?")
+// var verifyN = confirm ("Would you like numbers?")
+// var verifySC = confirm ("Would you like special characters")
+// var verifyNC = prompt ("How many characters would you like in the password?")  //this will have some sort if than statement confirming something is true reset
+
+// alert("Would you like lower-case letters?" + verifyLC )
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); ()
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+
+
+
+
+
+
+//  we would create a variable in here for generate password that would have like (lowercase, uppercase, number sp)
+}
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);{
+
+  alert("Would you like lower-case letters?" + verifyLC )
+
+
+}
+
+

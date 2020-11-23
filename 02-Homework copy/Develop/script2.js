bb3ddb23bcc6179ac54920582f82425ad4b83dbb
Francisco Rosado <Franciscorosado09@gmail.com>
2020-11-22 22:13:45 -0500

    updated with question, need to update math and refactor

--- 02-Homework copy/Develop/script2.js
+++ 02-Homework copy/Develop/script2.js
@@ -9,7 +9,7 @@ console.log(lowercase);
 var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");;
 console.log (uppercase);
 
-
+// var numbers= (math.floor(math.random() * 9)+1);
 var numbers = ("0123456789").split("");
 console.log (numbers);
 
@@ -17,71 +17,78 @@ console.log (numbers);
 var specialcharacters= ("!@#$%^&*+=").split("")
 console.log (specialcharacters)
 
+var passwordCombo = []
+
 
 // Write password to the #password input
 function writePassword() {
   
-
-//   var verifyNC = prompt ("How many characters would you like in the password?"); 
-//   var characterlength= parseInt(verifyNC);
-//     if(characterlength<= 128 && characterlength >= 8){
-//         // return true;
-//         //add something to add combo pw
-
-//     }
-
-// var verifyLC = confirm ("anything")
-//     if (confirmLowercase) {
-        
-//     } else {
-        
-//     };
-    
-//     else{
-//         alert("Your character length needs to be a minimum of 8 and maxmimum of 128.");
-//         // return false;
-//     };
-
+var verifyNC = prompt ("How many characters would you like in the password?"); 
+var characterlength= parseInt(verifyNC);
+  if(characterlength<= 128 && characterlength >= 8){
+      // return true;
+      //add something to add combo pw
+    console.log (verifyNC)
 
     
+      var verifyLC = confirm("Would you like lower-case letters?")
+      console.log (verifyLC)
+      if (verifyLC) {passwordCombo = passwordCombo.concat(lowercase);
+        console.log(passwordCombo)}
+          
+      
+
+      var verifyUC = confirm ("Would you like upper-case letters?")
+      console.log (verifyUC)
+      if (verifyUC) {passwordCombo = passwordCombo.concat(uppercase);
+        console.log(passwordCombo)}
+
+
+
+      var verifyN = confirm ("Would you like numbers?")
+      console.log (verifyN)
+      if (verifyN) {passwordCombo = passwordCombo.concat(numbers);
+        console.log(passwordCombo)}
+
+      
+
+
+      var verifySC = confirm ("Would you like special characters")
+      console.log (verifySC)
+      if (verifySC) {passwordCombo = passwordCombo.concat(specialcharacters);
+        console.log(passwordCombo)}
+
+
+
+        var password= "";
+        for (i = 0; i < characterlength; i++) {
+        var random = randomChoices [math.floor(math.random() * characterlength)];
+        password = characterlength[random]
+        console.log (password)}
 
   
+
   
 
 
+//   var password = generatePassword();
 
 
-  var password = generatePassword();
 
-  var verifyNC = prompt ("How many characters would you like in the password?"); 
-  var characterlength= parseInt(verifyNC);
-    if(characterlength<= 128 && characterlength >= 8){
-        // return true;
-        //add something to add combo pw
-
-    }
-
-// var verifyLC = confirm ("anything")
-//     if (confirmLowercase) {
-        
-//     } else {
-        
-//     };
     
-    else{
-        alert("Your character length needs to be a minimum of 8 and maxmimum of 128.");
-        // return false;
-    };
-    if (lowercase == true) {
-        var verifyLC = confirm ("anything")
-        
-    }
 
-  var passwordText = document.querySelector("#password");
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+//   passwordText.value = password
+} 
+else {
+    alert("Your character length needs to be a minimum of 8 and maxmimum of 128.");
+    // return false;
 };
 
+    
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{}
